7887ec081a2b9480690845b061fdb497de9823c0
mwomack <mikewomack2000@yahoo.com>
2019-03-04 14:57:55 -0800

    test

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const initialState = {};
 
@@ -11,7 +9,7 @@ const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  { initialState },
+  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
