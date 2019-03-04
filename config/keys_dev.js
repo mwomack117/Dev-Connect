5a6bb68e1bbbb6202863352fbbb5026da98f9e8b
mwomack <mikewomack2000@yahoo.com>
2019-03-04 13:04:16 -0800

    testing switch to atlas db

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://localhost/social-connect",
+  mongoURI:
+    "mongodb://mike123:mike123@mw-cluster-shard-00-00-szjc4.mongodb.net:27017,mw-cluster-shard-00-01-szjc4.mongodb.net:27017,mw-cluster-shard-00-02-szjc4.mongodb.net:27017/test?ssl=true&replicaSet=MW-Cluster-shard-0&authSource=admin&retryWrites=true",
   secretOrKey: "secret"
 };
