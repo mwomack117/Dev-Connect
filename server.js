f69c9138cbd69f898be58518f359feb71c1701f9
mwomack <mikewomack2000@yahoo.com>
2019-03-05 11:59:50 -0800

    testing deploy

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ app.use("/api/posts", posts);
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   // app.use(express.static("client/build"));
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
