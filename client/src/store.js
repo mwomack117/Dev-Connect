8b5985c8dfe21a6ec9e0b94898d2a4eee35ae19a
mwomack <mikewomack2000@yahoo.com>
2019-03-04 18:32:58 -0800

    test

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,20 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducers";
+
+// const initialState = {};
+
+// const middleware = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+// export default store;
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
@@ -7,10 +22,26 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
-
+if (
+  window.navigator.userAgent.includes("Chrome") &&
+  !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(
+    window.navigator.userAgent
+  )
+) {
+  var store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  var store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+}
 export default store;
