c8e3c5345743035b5b5176df4311f46e5e431615
mwomack <mikewomack2000@yahoo.com>
2019-03-06 16:55:40 -0800

    test deploy

--- config/passport.js
+++ config/passport.js
@@ -6,8 +6,7 @@ const keys = require("../config/keys");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-// opts.secretOrKey = keys.secretOrKey;
-opts.secretOrKey = "secret";
+opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
   passport.use(
--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // DB config
-// const db = require("./config/keys").mongoURI;
-const db =
-  process.env.MONGO_URI ||
-  "mongodb://mike123:mike123@mw-cluster-shard-00-00-szjc4.mongodb.net:27017,mw-cluster-shard-00-01-szjc4.mongodb.net:27017,mw-cluster-shard-00-02-szjc4.mongodb.net:27017/test?ssl=true&replicaSet=MW-Cluster-shard-0&authSource=admin&retryWrites=true";
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
